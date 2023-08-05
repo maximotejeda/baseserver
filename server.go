ff1be86e7f5e6636340732967a937c92b2bb3aef
maximo tejeda <maximotejeda@gmail.com>
2023-08-05 15:08:55 -0400

    ADD metrics endpoint for prometheus

--- server.go
+++ server.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
 
 func main() {
-	fmt.Println("base Server")
+	conf := preRun()
+	if conf["address"] == "" {
+		fmt.Printf("Server running on addr -> %s:%s\n", "localhost", conf["port"])
+	} else {
+		fmt.Printf("Server running on addr -> %s:%s\n", conf["address"], conf["port"])
+
+	}
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(conf["address"]+":"+conf["port"], nil))
+}
+
+// preRun check all necesary env to be present
+func preRun() map[string]string {
+	config := make(map[string]string)
+
+	config["address"] = os.Getenv("ADDR")
+	config["port"] = os.Getenv("PORT")
+
+	if config["port"] == "" {
+		log.Fatal("Port variable not set")
+	}
+	return config
 }
